2f07be11413d2acdfdb27322e0b32fb858a58360
Envy528 <carmonaballestasj528@gmail.com>
2026-06-02 07:22:04 -0500

    W3 - Task 3: Add notes to DOM

--- Week 3/main.js
+++ Week 3/main.js
@@ -1,5 +1,25 @@
+// HTML variables
 const btn = document.getElementById("agregarNota");
 const input = document.getElementById("nota");
-const salida = document.getElementById("listaNotas");
+const lista = document.getElementById("listaNotas");
 
-console.log(btn, input, salida)
+// Show variables in console - Task 2
+console.log(btn, input, lista);
+
+// Add event listener to the button - Task 3
+btn.addEventListener("click", function () {
+    //If the input is empty does this
+    if (!input.value) {
+        alert("Agrega algo en el input");
+    } else {
+        // if the input is not empty create a li element and adds it to the HTML
+        const item = document.createElement("li");
+        item.innerHTML = `${input.value} <button id="btnDel">Eliminar</button>`;
+        lista.appendChild(item);
+        // Clear the input and focus it
+        input.value = "";
+        input.focus();
+        // Shows on console the note added
+        console.log("Nota agregada: ", item.innerText);
+    }
+});
